4f1ec40f7c3f17d7fef57de6d91c7563edb2cd9d
Natalie <natalie@MacBook-Pro.local>
2020-05-30 12:21:33 +0200

    clock

---
+++ main.js
@@ -0,0 +1,105 @@
+var monate = [
+    "Januar",
+    "Februar",
+    "März",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember"
+    ];
+    
+    var wochenTag = [
+    "Sonntag",
+    "Montag",
+    "Dienstag",
+    "Mittwoch",
+    "Donnerstag",
+    "Freitag",
+    "Samstag"
+    ];
+
+
+    // let hour = document.getElementById("hour");
+    // let minutes = document.getElementById("minutes");
+    // let seconds = document.getElementById("seconds")
+    // let weekday = document.getElementById("weekday");
+    // let day = document.getElementById("day");
+    // let month = document.getElementById("month");
+    // let year = document.getElementById("year");
+
+
+    // let clock = new Date();
+
+
+   
+    // weekday.innerHTML = wochenTag [clock.getDay()]; 
+    // day.innerHTML = clock.getDate();
+    // month.innerHTML = monate[clock.getMonth()];
+
+    // year.innerHTML = clock.getFullYear();
+
+    // hour.innerHTML = clock.getHours(); 
+    // minutes.innerHTML = clock.getMinutes(); 
+    // seconds.innerHTML = clock.getSeconds();
+   
+    // if (hour.innerHTML <10) {
+    //     hour.innerHTML = "0" + clock.getHours()
+    // }
+
+    // if (minutes.innerHTML <10) {
+    //     minutes.innerHTML = "0" + clock.getMinutes();
+    // }
+
+
+
+    function updateClock() {
+        let hour = document.getElementById("hour");
+        let minutes = document.getElementById("minutes");
+        let seconds = document.getElementById("seconds")
+        let weekday = document.getElementById("weekday");
+        let day = document.getElementById("day");
+        let month = document.getElementById("month");
+        let year = document.getElementById("year");
+
+        let clock = new Date();
+    
+        weekday.innerHTML = wochenTag [clock.getDay()]; 
+        day.innerHTML = clock.getDate();
+        month.innerHTML = monate[clock.getMonth()];
+    
+        year.innerHTML = clock.getFullYear();
+    
+        hour.innerHTML = clock.getHours(); 
+        minutes.innerHTML = clock.getMinutes(); 
+        seconds.innerHTML = clock.getSeconds();
+       
+        if (hour.innerHTML <10) {
+            hour.innerHTML = "0" + clock.getHours()
+        }
+    
+        if (minutes.innerHTML <10) {
+            minutes.innerHTML = "0" + clock.getMinutes();
+        }
+    }
+    
+
+      
+      setInterval(updateClock, 1000)
+
+    // const startTime = () =>
+    // location.reload;
+    // console.log("updating...")
+
+    // setInterval(startTime, 1000);
+
+  
+
+    // seconds.innerHTML = clock.getSeconds(),1000);
+
+
+    // clock = new Date(), 1000);
